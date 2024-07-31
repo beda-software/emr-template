044876d3a8be6a7662a7bd55955c3b44a838163e
Vadim Laletin <rusmiligamer@gmail.com>
2024-07-31 11:21:13 +0200

    Add imports declarations for contrib

---
+++ src/imports.d.ts
@@ -0,0 +1,5 @@
+// This file is used exclusively by TS to suggest more natural imports
+// in additional to ../contrib/ imports
+import config from '@beda.software/emr-config';
+import '@beda.software/emr';
+import '@beda.software/aidbox-types';
