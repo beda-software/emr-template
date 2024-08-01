0e57732bad59b60fd6bb0b5d6e4a4f526623bbbf
Elena Grednikova <vesnushkalena@gmail.com>
2024-08-01 14:20:40 +0200

    Add PatientDashboardProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,8 @@ import '@beda.software/emr/dist/style.css';
 // You can specify your own theme to ajdust color,
 // Use you https://github.com/beda-software/fhir-emr/blob/master/src/theme/ThemeProvider.tsx as example
 import { App } from '@beda.software/emr/containers';
+import { PatientDashboardProvider } from '@beda.software/emr/dist/components/Dashboard/contexts';
+import { dashboard } from '@beda.software/emr/dist/dashboard.config';
 import { ThemeProvider } from '@beda.software/emr/theme';
 
 import { dynamicActivate, getCurrentLocale } from './services/i18n';
@@ -24,9 +26,11 @@ export const AppWithContext = () => {
 
     return (
         <I18nProvider i18n={i18n}>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
+            <PatientDashboardProvider dashboard={dashboard}>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </PatientDashboardProvider>
         </I18nProvider>
     );
 };
